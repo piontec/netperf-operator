4bf82c18c9e79325ad6e8bfcb9192352eec04e95
Łukasz Piątkowski <piontec@gmail.com>
2018-06-06 19:15:01 +0200

    update build files

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # for pushing th image from travis
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-VER=`grep "const version = " cmd/netperf-operator/main.go | cut -f2 -d'"'` GIT=`git rev-parse --short HEAD` docker push tailoredcloud/netperf-operator:v${VER}-${GIT} 
+export VER=`grep "const version = " cmd/netperf-operator/main.go | cut -f2 -d'"'` 
+export GIT=`git rev-parse --short HEAD` 
+echo "pushing version v${VER}-${GIT}"
+docker push tailoredcloud/netperf-operator:v${VER}-${GIT} 
