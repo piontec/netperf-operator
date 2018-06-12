64b8bf6b0502ebc8fa47dc933722991c9ef0cc52
Łukasz Piątkowski <piontec@gmail.com>
2018-06-12 22:06:40 +0200

    start test - stub

---
+++ pkg/stub/handler_test.go
@@ -0,0 +1,12 @@
+package stub
+
+import (
+	"testing"
+)
+
+func TestParseResults(t *testing.T) {
+	// h := &Handler{}
+	// if _, err := h.parseNetperfResult("random"); err != nil {
+	// 	t.Fail()
+	// }
+}
